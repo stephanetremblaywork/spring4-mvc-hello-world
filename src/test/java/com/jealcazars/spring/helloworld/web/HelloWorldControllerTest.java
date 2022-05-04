b8f5c3941b2325ac2f92fb2bd8790cd794df942c
Stephane Tremblay <steph.tremblay@cgi.com>
2022-05-04 11:54:56 -0400

    Fixed imports in unit tests and the pom file for dependencies.

--- src/test/java/com/jealcazars/spring/helloworld/web/HelloWorldControllerTest.java
+++ src/test/java/com/jealcazars/spring/helloworld/web/HelloWorldControllerTest.java
@@ -1,6 +1,6 @@
 package com.jealcazars.spring.helloworld.web;
 
-import static org.mockito.Matchers.anyString;
+import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
